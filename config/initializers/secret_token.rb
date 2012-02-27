1f255bc4808e842553d9b11ed130dfa434c7b355
Jeff Cohen <jeff@codeacademy.org>
2012-02-27 08:18:17 -0600

    Start the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Air::Application.config.secret_token = '29635cbe60073ea755aa1af68b7b6139f094047b3750ba432e4fee8685d9dfd2e3632a901d5ad26bcd2ae8a11a807fdf617944659a07774d874e707f3aa568a1'
